bf5ce0690dd8c8089059c09b31efb6e2659e4a68
Yuno0997 <156635106+Yuno0997@users.noreply.github.com>
2025-09-01 19:07:53 +0800

    added requirements

--- todolist/settings.py
+++ todolist/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -67,6 +68,8 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+DATABASES ["default"] = dj_database_url.parse("postgresql://todolist_django_render_io1t_user:EEUPjHSqi2RRuitG8QUhVj7WX2hvvoGI@dpg-d2qmmlre5dus73c8uo20-a.oregon-postgres.render.com/todolist_django_render_io1t")
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
